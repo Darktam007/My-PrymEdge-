eea35612f7aa1a3909a96dcea1aafb39c3c92f5f
Waribugo Godspower <163967188+Darktam007@users.noreply.github.com>
2026-05-25 20:03:05 +0100

    Update WhatsAppButton.tsx

--- src/Components/WhatsAppButton.tsx
+++ src/Components/WhatsAppButton.tsx
@@ -1,1 +1,13 @@
+import { FaWhatsapp } from "react-icons/fa";
 
+export default function WhatsAppButton() {
+  return (
+    <a
+      href="https://wa.me/qr/HTCW3QUKECDZJ1"
+      target="_blank"
+      className="whatsapp-button"
+    >
+      <FaWhatsapp size={28} />
+    </a>
+  );
+}
