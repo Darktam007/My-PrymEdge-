5b9c28e851777f83f500f0b962bf315acc85c7a5
Waribugo Godspower <163967188+Darktam007@users.noreply.github.com>
2026-05-25 20:02:10 +0100

    Add Navbar component with navigation links

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,1 +1,22 @@
+import { Link } from "wouter";
 
+export default function Navbar() {
+  return (
+    <header className="navbar">
+      <div
+        style={{
+          fontWeight: 700,
+          fontSize: "1.2rem",
+        }}
+      >
+        PrymEdge
+      </div>
+
+      <nav>
+        <Link href="/">Home</Link>
+        <Link href="/services">Services</Link>
+        <Link href="/contact">Contact</Link>
+      </nav>
+    </header>
+  );
+}
